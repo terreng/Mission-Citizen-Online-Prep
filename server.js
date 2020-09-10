f2ad86d3948c4daeca1266c5d54aee9a810d8009
Terren <11605395+terreng@users.noreply.github.com>
2020-09-09 19:31:16 -0700

    multiple choice questions

--- server.js
+++ server.js
@@ -626,10 +626,19 @@ if (query.index !== "NaN" && String(Number(query.index)) == query.index) {
   res.end();
   return;
 }
-if (body.option != null && body.option !== "NaN" && String(Number(body.option)) == body.option) {
+if (body.option != null) {
+if (typeof body.option == "object" && body.option.length > 0) {
+  body.option = body.option.filter(function(a) {return (a !== "NaN" && String(Number(a)) == a)}).map(function(a) {return Number(a)})
+  if (body.option.length == 0) {
+    body.option = false;
+  }
+} else {
+if (body.option !== "NaN" && String(Number(body.option)) == body.option) {
   body.option = Number(body.option);
 } else {
   body.option = false;
+}
+}
 }
 doAuthentication(cookies,function(userdata) {
 
@@ -649,34 +658,58 @@ if (userquizdata && (userquizdata.choices || []).length == query.index) {
       if (!questions_shuffled[question_index].answers || !(questions_shuffled[question_index].answers.length > 0)) {
         return internalServerError(undefined,true);
       }
+
+      var multiple = questions_shuffled[question_index].type || false;
+      if (multiple == 1) {
+        multiple = false;
+      }
   
       var options_shuffled = shuffleArray(questions_shuffled[question_index].answers,userquizdata.date+question_index);
   
       var selected_options = [];
-      var hasrightanswer = false;
-  
+      var hasrightanswer = 0;
+
       for (var i = 0; i < options_shuffled.length; i++) {
         if (options_shuffled[i].correct) {
-          if (hasrightanswer == false) {
-            hasrightanswer = true;
+          if (hasrightanswer !== (multiple || 1)) {
+            hasrightanswer++;
           } else {
             continue;
           }
         } else {
-          if (selected_options.length == 3 && !hasrightanswer) {
+          if (selected_options.length == (((multiple || 1)*4)-(multiple || 1)) && hasrightanswer !== (multiple || 1)) {
             continue;
           }
+        }
+        if (selected_options.length == (multiple || 1)*4) {
+          continue;
         }
         selected_options.push(options_shuffled[i]);
       }
   
-      if (!hasrightanswer) {
+      if (hasrightanswer !== (multiple || 1)) {
         return internalServerError(undefined,true);
       }
 
-      if (body.option !== false) {
+      if (body.option !== false && ((typeof body.option == "number" && !multiple) || (typeof body.option == "object" && body.option.length == (multiple || 1)))) {
 
-      admin.database().ref("users/"+cookies.code+"/quizzes/"+query.id+"/choices/"+query.index).set([body.option,selected_options[body.option].correct ? 1 : 0]).then(function() {
+      var iscorrect = false;
+      if (body.option) {
+      if (typeof body.option == "number") {
+        iscorrect = selected_options[body.option].correct || false;
+      } else {
+        if (body.option.length == multiple) {
+        iscorrect = true;
+        for (var i = 0; i < body.option.length; i++) {
+          if (selected_options[body.option[i]].correct !== true) {
+            iscorrect = false;
+          }
+        }
+        }
+      }
+      }
+
+      admin.database().ref("users/"+cookies.code+"/quizzes/"+query.id+"/choices/"+query.index).set([body.option,iscorrect ? 1 : 0]).then(function() {
 
         res.writeHead(302, {"Location": (process.env.NODE_ENV == "production" ? "https://" : "http://")+req.headers.host+"/lesson/"+(userquizdata.lessonindex+1)+"/quiz?id="+query.id+"&step="+((query.index*2)+1)});
         res.end();
@@ -842,13 +875,13 @@ if (userquizdata && !((query.step || 0) > userquizdata.length*2) && ((userquizda
 
     var pendhtml = "";
 
-    pendhtml += '<main><div><div class="question_subtitle">'+localize(reasoning ? localizations[cookies.lang].general.question_result_label : localizations[cookies.lang].general.question_label,cookies.lang,{"NUM":String(question_index+1), "TOTAL_NUM": String(questions_shuffled.length), "RESULT": (reasoning ? (selected_options[userquizdata.choices[userquizdata.choices.length-1][0]].correct ? '<span style="color: #689f38">'+localizations[cookies.lang].general.correct+'</span>' : '<span style="color: #d32f2f">'+localizations[cookies.lang].general.incorrect+'</span>') : '')})+'</div><div class="question_question">'+questions_shuffled[question_index].question[cookies.lang]+'</div>'+((questions_shuffled[question_index].subtitle && questions_shuffled[question_index].subtitle[cookies.lang] && questions_shuffled[question_index].subtitle[cookies.lang].length > 0) ? '<div class="question_question_subtitle">'+questions_shuffled[question_index].subtitle[cookies.lang]+'</div>' : '')+'<form action="/quiz_submit?id='+query.id+'&index='+question_index+'" method="POST"><div class="question_options'+(reasoning ? " reasoning" : "")+'">';
+    pendhtml += '<main><div><div class="question_subtitle">'+localize(reasoning ? localizations[cookies.lang].general.question_result_label : localizations[cookies.lang].general.question_label,cookies.lang,{"NUM":String(question_index+1), "TOTAL_NUM": String(questions_shuffled.length), "RESULT": (reasoning ? (userquizdata.choices[userquizdata.choices.length-1][1] == 1 ? '<span style="color: #689f38">'+localizations[cookies.lang].general.correct+'</span>' : '<span style="color: #d32f2f">'+localizations[cookies.lang].general.incorrect+'</span>') : '')})+'</div><div class="question_question">'+questions_shuffled[question_index].question[cookies.lang]+'</div>'+((questions_shuffled[question_index].subtitle && questions_shuffled[question_index].subtitle[cookies.lang] && questions_shuffled[question_index].subtitle[cookies.lang].length > 0) ? '<div class="question_question_subtitle">'+questions_shuffled[question_index].subtitle[cookies.lang]+'</div>' : '')+'<form action="/quiz_submit?id='+query.id+'&index='+question_index+'" method="POST"><div class="question_options'+(reasoning ? " reasoning" : "")+'">';
 
     for (var i = 0; i < selected_options.length; i++) {
-      pendhtml += '<div'+(reasoning ? (selected_options[i].correct ? ' class="correct"' : (userquizdata.choices[userquizdata.choices.length-1][0] == i ? ' class="incorrect"' : '')) : '')+'><div><input'+(reasoning ? (userquizdata.choices[userquizdata.choices.length-1][0] == i ? ' checked disabled' : ' disabled') : '')+' type="'+(multiple ? "checkbox" : "radio")+'" name="option" value="'+i+'" id="'+i+'"></div><div><label for="'+i+'">'+selected_options[i].answer[cookies.lang]+'</label>'+((selected_options[i].correct && reasoning && questions_shuffled[question_index].reasoning && questions_shuffled[question_index].reasoning[cookies.lang] && questions_shuffled[question_index].reasoning[cookies.lang].length > 0) ? '<div>'+questions_shuffled[question_index].reasoning[cookies.lang]+'</div>' : '')+'</div></div>'
+      pendhtml += '<div'+(reasoning ? (selected_options[i].correct ? ' class="correct"' : ((typeof userquizdata.choices[userquizdata.choices.length-1][0] == "number" ? userquizdata.choices[userquizdata.choices.length-1][0] == i : userquizdata.choices[userquizdata.choices.length-1][0].indexOf(i) > -1) ? ' class="incorrect"' : '')) : '')+'><div><input'+(reasoning ? ((typeof userquizdata.choices[userquizdata.choices.length-1][0] == "number" ? userquizdata.choices[userquizdata.choices.length-1][0] == i : userquizdata.choices[userquizdata.choices.length-1][0].indexOf(i) > -1) ? ' checked disabled' : ' disabled') : '')+' type="'+(multiple ? "checkbox" : "radio")+'" name="option" value="'+i+'" id="'+i+'"></div><div><label for="'+i+'">'+selected_options[i].answer[cookies.lang]+'</label>'+((selected_options[i].correct && reasoning && questions_shuffled[question_index].reasoning && questions_shuffled[question_index].reasoning[cookies.lang] && questions_shuffled[question_index].reasoning[cookies.lang].length > 0) ? '<div>'+questions_shuffled[question_index].reasoning[cookies.lang]+'</div>' : '')+'</div></div>'
     }
 
-    pendhtml += '</div>'+(reasoning ? "</form>" : "")+(query.error == "nooption" ? '<div class="question_error">'+localizations[cookies.lang].general.nooption+'</div>' : '')+'<div style="overflow:hidden;margin-top:12px;">'+(reasoning ? '<form action="/lesson/'+lessonnumber+'/quiz?id='+query.id+'&step='+(query.step+1)+'" method="POST">' : '')+'<input type="submit" value="'+(reasoning ? localizations[cookies.lang].general.continue : localizations[cookies.lang].general.submit)+'" style="width: 200px;float:right;">'+(reasoning ? '</form>' : '')+'</div>'+(reasoning ? '' : '</form>')+'</div></main>'
+    pendhtml += '</div>'+(reasoning ? "</form>" : "")+(query.error == "nooption" ? '<div class="question_error">'+([2,3].indexOf(questions_shuffled[question_index].type) > -1 ? localize(localizations[cookies.lang].general.nooption_multiple,cookies.lang,{"NUM":String(questions_shuffled[question_index].type)}) : localizations[cookies.lang].general.nooption)+'</div>' : '')+'<div style="overflow:hidden;margin-top:12px;">'+(reasoning ? '<form action="/lesson/'+lessonnumber+'/quiz?id='+query.id+'&step='+(query.step+1)+'" method="POST">' : '')+'<input type="submit" value="'+(reasoning ? localizations[cookies.lang].general.continue : localizations[cookies.lang].general.submit)+'" style="width: 200px;float:right;">'+(reasoning ? '</form>' : '')+'</div>'+(reasoning ? '' : '</form>')+'</div></main>'
 
     fs.readFile("index.html", 'utf8', function(error, data) {
       if (error) {
